e6ce7421770f2ff3879d7cfb32a8b396ddefa950
SolomonVP <Lum7023@yandex.ru>
2023-01-21 00:37:35 +0300

    test ctrlC ctrlV

---
+++ src/main/java/com/jenk/MyController.java
@@ -0,0 +1,12 @@
+package com.jenk;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MyController {
+    @RequestMapping("/hello")
+    public String showHelloView() {
+        return "helloView";
+    }
+}
--- target/Deleted/index.jsp
+++ target/Deleted/index.jsp
@@ -1,5 +1,6 @@
 <html>
 <body>
-<h2>Hello World!!!!!!!!!!!!!!!!!!!</h2>
+<h2>It's first view... 0_o</h2>
+<h2>You need to Enter a browser in the string: "/hello"</h2>
 </body>
 </html>
